fc3de4db2613369a8f6e48a8aec2b5013d407e39
csd11 <36788845+csd11@users.noreply.github.com>
2018-02-24 15:51:01 +0530

    first commit
    sum of given integer

---
+++ sum of num/SON.java
@@ -0,0 +1,14 @@
+class SON{
+
+	public static void main(String args()){
+	
+		int sum = 0;
+		
+		for(int num=1; num<=10; num++){
+			
+			sum = sum + num;
+		}
+		
+		System.out.println("Sum of numbers = " + sum);
+	}	
+}
